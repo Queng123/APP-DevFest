13510df86a7043a82c02d03625f085356151445e
FoxaxeWasTaken <axel.idoux@epitech.eu>
2023-10-13 18:17:22 +0200

    feat(main): use arguments for ip and port

--- pi-display/Main.cpp
+++ pi-display/Main.cpp
@@ -11,9 +11,13 @@
 #include "ThreadedGameDatasDisplay.hpp"
 #include "ThreadedIOHandler.hpp"
 
-int main(void)
+int main(int ac, char *av[])
 {
-    Network::ThreadedUDPClient<Game::GameDatas, Game::IOInfos> client;
+    if (ac != 3) {
+        std::cerr << "Usage: " << av[0] << " <ip> <port>" << std::endl;
+        return 84;
+    }
+    Network::ThreadedUDPClient<Game::GameDatas, Game::IOInfos> client(av[1], std::atoi(av[2]));
     Display::ThreadedGameDatasDisplay display(*client);
     IO::ThreadedIOHandler ioHandler;
 
